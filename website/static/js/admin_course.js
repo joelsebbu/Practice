325e1ee2bf8f37e0dfeff5c7a7058aa58b5b846f
joelsebbu <joel18sebastian@gmail.com>
2022-08-28 22:23:01 +0530

    course cards remain but reads data

---
+++ website/static/js/admin_course.js
@@ -0,0 +1,23 @@
+// var instance = M.Tabs.init(el);
+// document.addEventListener('DOMContentLoaded', function() {
+//     var el = document.querySelectorAll('.tabs');
+//     var instance = M.Tabs.init(el);
+//   });
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems);
+//   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return 'home'
+    data ={
+        "category":["code","arts","buisness","crypto"],
+        "status":["active","disabled"],
+        "instructor":["1","2"]
+    }
+    return render_template('admin_course.html',data =data)
